52ad1993ccbc2fc54587968b23e8b1942c7e193e
Ståle <staale_80@hotmail.com>
2025-06-01 15:02:30 +0000

    Sending fake token

--- app/main.py
+++ app/main.py
@@ -1,17 +1,29 @@
 from fastapi.exceptions import RequestValidationError
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
+from fastapi.middleware.cors import CORSMiddleware
 from app.database import init_db
 from contextlib import asynccontextmanager
 from app.routes.users import router as users_router
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     init_db()
     yield
 
+
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(request: Request, exc: RequestValidationError):
@@ -20,8 +32,10 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
         content={"errors": jsonable_encoder(exc.errors())},
     )
 
+
 @app.get("/")
 def read_root():
     return {"message": "Hello from Dockerized FastAPI"}
+
 
 app.include_router(users_router)
--- app/routes/users.py
+++ app/routes/users.py
@@ -2,7 +2,13 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.database import get_db
 from app.models.models import User, Friendship
-from app.schemas.schemas import UserCreate, UserOut, UserLogin, FriendshipOut,FriendshipCreate
+from app.schemas.schemas import (
+    UserCreate,
+    UserOut,
+    UserLogin,
+    FriendshipOut,
+    FriendshipCreate,
+)
 from app.security import hash_password, verify_password
 from uuid import UUID
 
@@ -10,8 +16,9 @@ router = APIRouter(prefix="/users", tags=["users"])
 
 
 @router.get("/", response_model=list[UserOut])
-def get_all_users(db: Session= Depends(get_db)):
+def get_all_users(db: Session = Depends(get_db)):
     return db.query(User).all()
+
 
 @router.post("/", response_model=UserOut)
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
@@ -27,6 +34,7 @@ def create_user(user: UserCreate, db: Session = Depends(get_db)):
     db.refresh(new_user)
     return new_user
 
+
 @router.post("/login")
 def login(user: UserLogin, db: Session = Depends(get_db)):
     normalized_email = user.email.lower()
@@ -35,32 +43,40 @@ def login(user: UserLogin, db: Session = Depends(get_db)):
         raise HTTPException(status_code=401, detail="Invalid email or password")
     if not bool(db_user.is_verified):
         raise HTTPException(status_code=403, detail="Email not verified.")
-    return {"message": "Login successful"}
+    fake_token = "abc123token"
+
+    return {"token": fake_token, "message": "Login successful"}
+
 
 @router.post("/{user_id}/friends", response_model=FriendshipOut)
-def send_friend_request(user_id: UUID, request: FriendshipCreate, db: Session = Depends(get_db)):
+def send_friend_request(
+    user_id: UUID, request: FriendshipCreate, db: Session = Depends(get_db)
+):
     if user_id == request.friend_id:
         raise HTTPException(status_code=400, detail="You cannot friend yourself.")
-    
-    #Check if friend exists
+
+    # Check if friend exists
     target_user = db.query(User).filter(User.id == request.friend_id).first()
     if not target_user:
         raise HTTPException(status_code=404, detail="User not found.")
-    
-    #Check for existing friendship
-    existing = db.query(Friendship).filter(
-        Friendship.user_id == user_id,
-        Friendship.friend_id == request.friend_id
-    ).first()
+
+    # Check for existing friendship
+    existing = (
+        db.query(Friendship)
+        .filter(
+            Friendship.user_id == user_id, Friendship.friend_id == request.friend_id
+        )
+        .first()
+    )
 
     if existing:
-        raise HTTPException(status_code=400, detail="Friend request already sent or already friends")
-    
-    #Create friendship request
+        raise HTTPException(
+            status_code=400, detail="Friend request already sent or already friends"
+        )
+
+    # Create friendship request
     friendship = Friendship(
-        user_id = user_id,
-        friend_id = request.friend_id,
-        status="pending"
+        user_id=user_id, friend_id=request.friend_id, status="pending"
     )
     db.add(friendship)
     db.commit()
